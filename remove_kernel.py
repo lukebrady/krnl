06274061c81afa038a92e2658f43ea455fd7f913
Luke Brady <lbrad23105@gmail.com>
2017-12-30 00:00:03 -0500

    Changes to remove_kernel.py.

---
+++ remove_kernel.py
@@ -0,0 +1,18 @@
+import os
+
+from redis import redis_client
+
+
+def remove_kernel(version):
+    # Get a redis client object
+    client = redis_client.KrnlRedisClient().get_client()
+    kernel_path = './kernels/linux-' + version + '.tar.xz'
+
+    if os.path.exists(kernel_path):
+        os.remove(kernel_path)
+        client.lrem('_all', -1, version)
+        exit(code=0)
+    else:
+        print('ERROR: The kernel does not exist')
+        print('INFO: Could not remove kernel version ' + version)
+        exit(code=1)
